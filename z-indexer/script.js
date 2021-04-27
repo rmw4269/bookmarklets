bd235f40ee15f62a44c34934b8911f98d6b56f7d
Riley Mott Williams <rmw4269@rit.edu>
2021-04-26 21:13:57 -0400

    refactor z-indexer
    Z-indexer has been massively restructured. It’s no longer really inefficient, and it is no longer one line. Also, there are a few comments to help with following the logic. This should now work in browsers that lack support for `console.table`, `console.dir`, and the CSS typed object model.

--- z-indexer/script.js
+++ z-indexer/script.js
@@ -1,16 +1,45 @@
 javascript: {
-[...(document.querySelector(window.prompt("Enter the selector of the element whose children you want to inspect. The default is the document’s body.", "") || ":root > body") ?? document.body).children]
-	.map(elm => Object.assign(Object.create(null), {element: elm, "z-index": elm.computedStyleMap().get("z-index")}))
-	.filter(entry => entry["z-index"] instanceof CSSUnitValue)
-	.sort(
-		window.confirm("Would you like to sort the elements by z-index?")
-			? (({"z-index": {value: indexA}}, {"z-index": {value: indexB}}) => (indexA === indexB) ? 0 : (indexA < indexB) ? -1 : +1)
-			: () => 0
-	)
-	.reduce(
-		console.table
-			? (output, {element, "z-index": {value: zIndex}}, index, original) => {output.push(Object.assign(Object.create(null), {element, "z-index": zIndex})); if (index === original.length - 1) { console.table(output); } return output; }
-			: (map, {element, "z-index": {value: zIndex}}, index, original) => { map.set(element, zIndex); if (index === original.length - 1) { console.dir(map); } return map; },
-		console.table ? [] : new Map()
-	)
+	let parent;
+	try { parent = document.querySelector(window.prompt("Enter the selector of the element whose children you want to inspect. The default is the document’s body. Only the first result is used.", "") || ":root > body"); }
+	catch (e) { if (e instanceof DOMException) {
+		window.alert("Sorry, that is not a valid selector.");
+	} else { throw e; }}
+
+	if (parent) {
+		let zIndexIndex = [...parent.children]
+			.map(elm => Object.assign(Object.create(null), {element: elm, "z-index": elm.computedStyleMap().get("z-index")}))
+			.filter(
+				(typeof CSSUnitValue === "undefined")
+					/* test for a fully numeric string */
+					? entry => entry["z-index"].toString().trim().match(/[0-9]+/)
+					/* test for a CSS unit, rather than a keyword */
+					: entry => entry["z-index"] instanceof CSSUnitValue
+			);
+
+		zIndexIndex.forEach(
+			(typeof CSSUnitValue === "undefined")
+				/* parse the number strings into integers */
+				? entry => Object.assign(entry, {"z-index": Number.parseInt(entry["z-index"].trim())})
+				/* replace CSSUnitValue objects with their value */
+				: entry => Object.assign(entry, {"z-index": entry["z-index"].value})
+		);
+
+		/* optionally sort the elements by z-index */
+		if (window.confirm("Sort the elements by z-index?")) {
+			zIndexIndex.sort(({"z-index": indexA}, {"z-index": indexB}) => (indexA === indexB) ? 0 : (indexA < indexB) ? -1 : +1)
+		}
+
+		/* find some way to print out the results */
+		if (console.table) {
+			console.table(zIndexIndex);
+		} else if (console.dir) {
+			console.dir(new Map(zIndexIndex.map(({element, "z-index": zIndex}) => [element, zIndex])));
+		} else {
+			console.group ? console?.group("z-indices") : console.log("z-indices:")
+			for (let entry of zIndexIndex) {
+				console.log(entry.element + ": \t" + entry["z-index"]);
+			}
+			console.group && console.groupEnd();
+		}
+	} else { console.error("[z-indexer]\tNo elements were found, or the selector was invalid."); }
 }
